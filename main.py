554d308130d3f13f833f407471ccf7e43d1e9334
ar3ac <luca.marrazzo@gmail.com>
2026-03-15 15:38:04 +0100

    removed view.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import random
-from view import display_welcome_message
+
+
+def display_welcome_message():
+    print("\nWelcome to the Number Guessing Game!\n")
+    print("I'm thinking of a number between 1 and 100.")
+    print("Please select the difficulty level:\n")
+    print("1. Easy (10 chances)")
+    print("2. Medium (5 chances)")
+    print("3. Hard (3 chances)\n")
 
 
 def get_user_guess():
--- view.py
+++
@@ -1,7 +0,0 @@
-def display_welcome_message():
-    print("\nWelcome to the Number Guessing Game!\n")
-    print("I'm thinking of a number between 1 and 100.")
-    print("Please select the difficulty level:\n")
-    print("1. Easy (10 chances)")
-    print("2. Medium (5 chances)")
-    print("3. Hard (3 chances)\n")
